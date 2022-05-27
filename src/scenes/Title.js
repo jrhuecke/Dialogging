6501c160bc07d9264465df87953be0b6f6ed5121
Ryan Hueckel <jrhuecke@ucsc.edu>
2022-05-27 12:43:24 -0700

    intitial

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -9,6 +9,7 @@ class Title extends Phaser.Scene {
 
         // load JSON (dialog)
         this.load.json('dialog', 'json/dialog.json');
+        this.load.json('dialog2', 'json/dialog2.json');
 
         // load images
         this.load.image('dialogbox', 'img/dialogbox.png');
@@ -16,6 +17,10 @@ class Title extends Phaser.Scene {
         this.load.image('minerva', 'img/minerva.png');
         this.load.image('jove', 'img/jove.png');
         this.load.image('neptune', 'img/neptune.png');
+        this.load.image('duncan', 'img/duncan.png');
+        this.load.image('macbeth', 'img/macbeth.png');
+        this.load.image('banquo', 'img/banquo.png');
+        this.load.image('malcom', 'img/malcom.png');
 
         // load bitmap font
         this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml');
@@ -33,7 +38,7 @@ class Title extends Phaser.Scene {
     update() {
         // wait for player input
         if(Phaser.Input.Keyboard.JustDown(cursors.space)) {
-            this.scene.start("talkingScene");
+            this.scene.start("macbethScene");
         }
     }
 }
